ffdc102d2d2ad9dff380bba796ba58839576b236
qwer97 <mcrohmh@naver.com>
2024-07-17 19:06:10 +0900

    프로젝트 초기 코드

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport'); // passport 모듈을 추가
+const passportConfig = require('./config/passport');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const crypto = require('crypto');
+
+
+const app = express();
+
+// 세션 설정, 시크릿키는 javascript내부 메소드로 구현했습니다.
+const secret = crypto.randomBytes(64).toString('hex');
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Passport 초기화
+passportConfig();
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 라우트 설정
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+
+
+app.get('/oauth/google',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+      res.redirect('/profile');
+    }
+  );
+// 서버 실행
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Login with Google</a>');
+});
+
+router.get('/profile', (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    res.send(`안녕하세요, ${req.user.displayName}님!`);
+});
+
+router.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+// MySQL 연결 설정, 개개인마다 다를겁니다.
+const connection = mysql.createConnection({
+  host: 'localhost',  // MySQL 호스트 주소
+  user: 'root',   // MySQL 사용자 이름
+  password: '1112',  // MySQL 비밀번호
+  database: 'ATTI'  // ATTI 데이터베이스 이름
+});
+
+// MySQL 연결
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+
+module.exports = router;
