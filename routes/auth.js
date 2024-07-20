7966611cf01cf538ccd8392f3d1dbe6d960431e7
qwer97 <mcrohmh@naver.com>
2024-07-20 22:40:06 +0900

    jwt 방식으로 변환중

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-router.get('/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-  );
-  
-  router.get('/google/callback', 
+// Google 로그인
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', 
     passport.authenticate('google', { failureRedirect: '/login' }),
     (req, res) => {
-      res.redirect('/profile');
+        const token = jwt.sign(
+            { id: req.user.id, displayName: req.user.displayName }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' }
+        );
+        res.redirect(`/profile?token=${token}`);
     }
-  );
+);
  
   //카카오 로그인
 router.get('/kakao',
