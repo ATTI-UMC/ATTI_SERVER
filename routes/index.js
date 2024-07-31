d007eb1743743b60ffbf5a35d863e244f0822e84
임도현 <162318258+limdodod@users.noreply.github.com>
2024-07-31 13:22:10 +0900

    notification 추가

--- routes/index.js
+++ routes/index.js
@@ -107,4 +107,36 @@ router.get('/user/:id', (req, res) => {
   });
 });
 
+// 알림 생성 API
+router.post('/notifications', (req, res) => {
+  const { user_id, message } = req.body;
+  const query = 'INSERT INTO Notification (user_id, message) VALUES (?, ?)';
+  connection.query(query, [user_id, message], (err, result) => {
+      if (err) {
+          res.status(500).json({ error: err.message });
+      } else {
+          res.status(201).json({ notification_id: result.insertId });
+      }
+  });
+});
+
+// 알림 조회 API
+router.get('/notifications/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  connection.query('SELECT * FROM Notification WHERE user_id = ?', [user_id], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    else if (results.length === 0) {
+      return res.status(404).json({ error: 'No notifications found' });
+    }
+    else{
+    res.status(200).json(results);
+    }
+  });
+});
+
+
 module.exports = router;
